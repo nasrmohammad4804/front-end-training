b9b4c196ad885639268eafbd9f804bc4ad7917fa
mohammad <nasrmohammad4804@gmail.com>
2023-10-13 20:38:49 +0330

    add footer component with simple style

---
+++ hw7/template-app/src/components/footer/index.jsx
@@ -0,0 +1,7 @@
+import './main.css';
+
+export default function Footer(){
+    return <footer className='footer'>
+        copyright © 2022 Food For Life | All Rights Reserved | Privacy Policy | Term and Conditions | Cookie Policy | Accessibility Professional Website Development By Trinet Internet Solution, Inc.
+    </footer>
+}
