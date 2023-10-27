4f02f8d8994d65597984e50965585cae374e7003
mohammad <nasrmohammad4804@gmail.com>
2023-10-27 21:04:52 +0330

    add pages of wallpaper app

---
+++ hw9/wallpaper-app/src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import Header from "../../layouts/Header";
+import Main from "../../layouts/Main";
+
+export default function Home(){
+
+    return (
+        <>
+        <Header/>
+        <Main/>
+        </>
+    )
+    
+}
